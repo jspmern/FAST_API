56d62c090191fb892264d9db97ee317ecb1fd8cf
jspmern <jspmern@gmail.com>
2026-08-13 22:28:51 +0530

    this is the utilits for the write the file inside json

--- utilis.py
+++ utilis.py
@@ -7,3 +7,12 @@ def read_json():
             return data
         except json.JSONDecodeError:
             return {"error": "Invalid JSON in data file"}
+
+#this is a function to write the data to the json file and return the data in json format
+def write_json(data):
+    with open ("asset/data.json","w" ) as f:
+        try:
+            json.dump(data,f,indent=4)
+            return data
+        except Exception as e:
+            return {"error": str(e)}
