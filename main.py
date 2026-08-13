f7b1ec67bc4572c511f3e643a47e9159eeab7ee9
jspmern <jspmern@gmail.com>
2026-08-13 22:05:14 +0530

    added first route for health

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
 #initialize the FastAPI app
 app = FastAPI()
+
+
+#this is a health check endpoint to check if the application is running or not and we can run by uvicorn main:app --reload and we can check swagger document at localhost:8000/docs
+@app.get("/health")
+async def health_check():
+    return {"success":True,"message":"Application is up and running at 8000"}
