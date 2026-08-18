0f35fb30e2b1318e1b3549d0d78636b43d98f120
jspmern <jspmern@gmail.com>
2026-08-18 14:23:06 +0530

    throw exception

--- status.py
+++ status.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,status
+from fastapi import FastAPI,status,HTTPException
 #creating the app 
 app=FastAPI()
 
@@ -27,4 +27,11 @@ def gethealth():
 #send status code manually
 @app.get('/user',status_code=status.HTTP_201_CREATED)
 def getUser():
+    return employee
+
+#http expection also we can send form fast api
+@app.get('/studend/{id}')
+def getstudentData(id:str):
+    if id=="utsav":
+        raise HTTPException(status_code=404,detail="this user is not allowed")
     return employee
