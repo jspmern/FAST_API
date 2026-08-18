1b40e56c903c43da2abf33cf27a3782809a14767
jspmern <jspmern@gmail.com>
2026-08-18 15:30:51 +0530

    check noraml depends and  even dependecies take request data also, real time example with authentication and authorization

---
+++ dependency.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI,Depends,Header,HTTPException
+#app initialization
+app=FastAPI()
+@app.get('/')
+def checkHealth():
+    return {"message":"okay"}
+
+#basic dependency 
+# def greet():
+#     return "Hello how are you"
+
+# @app.get('/user')
+# def getUserInfo(user=Depends(greet)):
+#     return user
+
+
+#dependencies can take request data  
+# def get_user(user_id:int):
+#     return {"userId":user_id}
+# @app.get('/user/{userId}')
+# def getUserInfo(user=Depends(get_user)):
+#     return user
+
+
+# real time example 
+
+#a.for token extraction
+# def get_api_key(token:str=Header()):
+#     return token
+# @app.get('/user')
+# def userTokenInfo(token=Depends(get_api_key)):
+#     return token
+
+#b.for authorization 
+# def requrie_admin():
+#     userInfo={
+#         "username":"utsav",
+#         "role":"admin"
+#     }
+#     if userInfo['role'] != "admin":
+#         raise HTTPException(status_code=403,detail="Admin access require")
+#     return userInfo
+# @app.get('/user')
+# def deleteInfo(user=Depends(requrie_admin)):
+#     return {"message":"Deleted"}
